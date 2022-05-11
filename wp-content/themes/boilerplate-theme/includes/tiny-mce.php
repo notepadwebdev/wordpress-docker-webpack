e6f550da2c9bd6a9e0563bc9e322a461dc502cb5
Stephen Griffin <notepadwebdev@gmail.com>
2022-05-11 09:07:59 +0100

    Custom WYSIWYG toolbar options.

--- wp-content/themes/boilerplate-theme/includes/tiny-mce.php
+++ wp-content/themes/boilerplate-theme/includes/tiny-mce.php
@@ -53,4 +53,37 @@ function my_mce_buttons_2($buttons) {
   return $buttons;
 }
 
+
+// Custom WYSIWYG toolbars.
+add_filter( 'acf/fields/wysiwyg/toolbars' , 'my_toolbars'  );
+function my_toolbars( $toolbars )
+{
+	// Uncomment to view format of $toolbars
+	//
+	// echo '< pre >';
+	// 	print_r($toolbars);
+	// echo '< /pre >';
+	// die;
+	//
+
+	// Add a new toolbar called "Very Simple"
+	// - this toolbar has only 1 row of buttons
+	$toolbars['Headings' ] = array();
+	$toolbars['Headings' ][1] = array('formatselect', 'alignleft', 'aligncenter', 'alignright', 'wp_adv');
+  $toolbars['Headings' ][2] = array('styleselect');
+
+	// Edit the "Full" toolbar and remove 'code'
+	// - delet from array code from http://stackoverflow.com/questions/7225070/php-array-delete-by-value-not-key
+	if( ($key = array_search('code' , $toolbars['Full' ][2])) !== false )
+	{
+	  unset( $toolbars['Full' ][2][$key]);
+	}
+
+	// remove the 'Basic' toolbar completely
+	// unset( $toolbars['Basic' ] );
+
+	// return $toolbars - IMPORTANT!
+	return $toolbars;
+}
+
 ?>
