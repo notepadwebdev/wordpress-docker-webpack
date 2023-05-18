bba94bb7daea6a7d159c737e27b34bd59816775a
Stephen Griffin <notepadwebdev@gmail.com>
2023-05-18 11:03:42 +0100

    Comment out default Products CPT.

--- wp-content/themes/boilerplate-theme/includes/custom-post-types.php
+++ wp-content/themes/boilerplate-theme/includes/custom-post-types.php
@@ -24,43 +24,43 @@
 // }
 
 // Products.
-add_action('init', 'products_register');
-function products_register() {
-  $labels = array(
-    'name' => _x('Products', 'post type general name'),
-    'singular_name' => _x('Product', 'post type singular name'),
-    'add_new' => _x('Add New', 'Product'),
-    'add_new_item' => __('Add New Product'),
-    'edit_item' => __('Edit Product'),
-    'new_item' => __('New Product'),
-    'view_item' => __('View Product'),
-    'search_items' => __('search Products'),
-    'not_found' =>  __('nothing found'),
-    'not_found_in_trash' => __('nothing found in Trash'),
-    'parent_item_colon' => ''
-  );
-  $args = array(
-    'labels' => $labels,
-    'public' => true,
-    'publicly_queryable' => true,
-    'show_ui' => true,
-    'query_var' => true,
-    'rewrite' => array(
-      'slug' => 'products',
-      'with_front' => false,
-    ),
-    'capability_type' => 'page',
-    'has_archive' => false,
-    'hierarchical' => false,
-    'menu_position' => 0,
-    'supports' => array('title', 'thumbnail', 'editor', 'author'),
-    'taxonomies' => array(),
-    'show_in_rest' => true,
-    'show_in_graphql' => true,
-    'graphql_single_name' => 'product',
-    'graphql_plural_name' => 'products',
-  );
-  register_post_type('products' , $args);
-  flush_rewrite_rules();
-}
+// add_action('init', 'products_register');
+// function products_register() {
+//   $labels = array(
+//     'name' => _x('Products', 'post type general name'),
+//     'singular_name' => _x('Product', 'post type singular name'),
+//     'add_new' => _x('Add New', 'Product'),
+//     'add_new_item' => __('Add New Product'),
+//     'edit_item' => __('Edit Product'),
+//     'new_item' => __('New Product'),
+//     'view_item' => __('View Product'),
+//     'search_items' => __('search Products'),
+//     'not_found' =>  __('nothing found'),
+//     'not_found_in_trash' => __('nothing found in Trash'),
+//     'parent_item_colon' => ''
+//   );
+//   $args = array(
+//     'labels' => $labels,
+//     'public' => true,
+//     'publicly_queryable' => true,
+//     'show_ui' => true,
+//     'query_var' => true,
+//     'rewrite' => array(
+//       'slug' => 'products',
+//       'with_front' => false,
+//     ),
+//     'capability_type' => 'page',
+//     'has_archive' => false,
+//     'hierarchical' => false,
+//     'menu_position' => 0,
+//     'supports' => array('title', 'thumbnail', 'editor', 'author'),
+//     'taxonomies' => array(),
+//     'show_in_rest' => true,
+//     'show_in_graphql' => true,
+//     'graphql_single_name' => 'product',
+//     'graphql_plural_name' => 'products',
+//   );
+//   register_post_type('products' , $args);
+//   flush_rewrite_rules();
+// }
 ?>
