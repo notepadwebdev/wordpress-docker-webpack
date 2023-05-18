a5460769f9c0e55e330c098f25a12d76aa958548
Stephen Griffin <notepadwebdev@gmail.com>
2023-05-18 11:18:09 +0100

    Group typography sizes in WYSIWYG formats into a sub dropdown.

--- wp-content/themes/boilerplate-theme/includes/tiny-mce.php
+++ wp-content/themes/boilerplate-theme/includes/tiny-mce.php
@@ -6,41 +6,46 @@ function my_mce_before_init_insert_formats($init_array) {
 
   $style_formats = array(
     array(
-      'title' => 'Lede',
-      'block' => 'div',
-      'classes' => 'lede',
-      'wrapper' => true,
-    ),
-    array(
-      'title' => 'f-h1-size',
-      'selector' => 'h1,h2,h3,h4,h5,h6,p,div',
-      'classes' => 'h1'
-    ),
-    array(
-      'title' => 'f-h2-size',
-      'selector' => 'h1,h2,h3,h4,h5,h6,p,div',
-      'classes' => 'h2'
-    ),
-    array(
-      'title' => 'f-h3-size',
-      'selector' => 'h1,h2,h3,h4,h5,h6,p,div',
-      'classes' => 'h3'
-    ),
-    array(
-      'title' => 'f-h4-size',
-      'selector' => 'h1,h2,h3,h4,h5,h6,p,div',
-      'classes' => 'h4'
-    ),
-    array(
-      'title' => 'f-h5-size',
-      'selector' => 'h1,h2,h3,h4,h5,h6,p,div',
-      'classes' => 'h5'
-    ),
-    array(
-      'title' => 'f-h6-size',
-      'selector' => 'h1,h2,h3,h4,h5,h6,p,div',
-      'classes' => 'h6'
-    ),
+      'title' => 'Typography Sizes',
+      'items' => array(
+        array(
+          'title' => 'H1',
+          'selector' => 'h1,h2,h3,h4,h5,h6,p,div',
+          'classes' => 'h1'
+        ),
+        array(
+          'title' => 'H2',
+          'selector' => 'h1,h2,h3,h4,h5,h6,p,div',
+          'classes' => 'h2'
+        ),
+        array(
+          'title' => 'H3',
+          'selector' => 'h1,h2,h3,h4,h5,h6,p,div',
+          'classes' => 'h3'
+        ),
+        array(
+          'title' => 'H4',
+          'selector' => 'h1,h2,h3,h4,h5,h6,p,div',
+          'classes' => 'h4'
+        ),
+        array(
+          'title' => 'H5',
+          'selector' => 'h1,h2,h3,h4,h5,h6,p,div',
+          'classes' => 'h5'
+        ),
+        array(
+          'title' => 'H6',
+          'selector' => 'h1,h2,h3,h4,h5,h6,p,div',
+          'classes' => 'h6'
+        ),
+        array(
+          'title' => 'Lede',
+          'block' => 'div',
+          'classes' => 'lede',
+          'wrapper' => true,
+        ),
+      )
+    )
   );
   $init_array['style_formats'] = json_encode($style_formats);
   return $init_array;
