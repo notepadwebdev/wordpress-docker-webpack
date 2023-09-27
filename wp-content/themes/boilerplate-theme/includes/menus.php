78eb15eb84bad4c9fe98987d809a8385ef766d45
Stephen Griffin <notepadwebdev@gmail.com>
2023-09-27 08:39:20 +0100

    Reusable blocks menu item in CMS.

--- wp-content/themes/boilerplate-theme/includes/menus.php
+++ wp-content/themes/boilerplate-theme/includes/menus.php
@@ -22,6 +22,16 @@ function admin_clean() {
 }
 
 /**
+ * Reusable Blocks accessible in backend
+ * @link https://www.billerickson.net/reusable-blocks-accessible-in-wordpress-admin-area
+ *
+ */
+function be_reusable_blocks_admin_menu() {
+  add_menu_page( 'Reusable Blocks', 'Reusable Blocks', 'edit_posts', 'edit.php?post_type=wp_block', '', 'dashicons-editor-table', 22 );
+}
+add_action( 'admin_menu', 'be_reusable_blocks_admin_menu' );
+
+/**
  * Control order of Admin Menu items.
  */
 add_filter( 'custom_menu_order', 'reorder_admin_menu' );
