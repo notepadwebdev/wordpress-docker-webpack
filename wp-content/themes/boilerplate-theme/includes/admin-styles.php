3e53314c2185b3af6257d901f9e36052a0c59aa0
Stephen Griffin <notepadwebdev@gmail.com>
2024-05-19 10:43:10 +0100

    Fix admin styles core CSS issue.

--- wp-content/themes/boilerplate-theme/includes/admin-styles.php
+++ wp-content/themes/boilerplate-theme/includes/admin-styles.php
@@ -1,14 +1,31 @@
 <?php
 
 add_theme_support( 'editor-styles' );
+add_editor_style( 'dist/css/styles.css' );
+add_editor_style( 'admin.css' );
 
 add_action('admin_head', 'theme_admin_styles');
 function theme_admin_styles() {
  echo '<style>
+  html :where(.wp-block) {
+    margin: 0 !important;
+  }
   .editor-styles-wrapper {
     background-color: white !important;
   }
  </style>';
 }
+
+// Remove ACF inline styles for WYSIWYG.
+function my_acf_input_admin_footer() { ?>
+	<script type="text/javascript">
+		(function($) {
+			acf.add_action('wysiwyg_tinymce_init', function( ed, id, mceInit, $field ){
+				$(".acf-field .acf-editor-wrap iframe").removeAttr("style");
+			});
+		})(jQuery);	
+	</script>
+<?php }
+add_action('acf/input/admin_footer', 'my_acf_input_admin_footer');
 
 ?>
