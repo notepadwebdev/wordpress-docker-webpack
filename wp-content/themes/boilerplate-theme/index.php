095a78cf14b016af0b975e47fc1dafb678c07d04
Stephen Griffin <notepadwebdev@gmail.com>
2024-06-06 16:41:00 +0100

    Index.php for theme amend.

--- wp-content/themes/boilerplate-theme/index.php
+++ wp-content/themes/boilerplate-theme/index.php
@@ -1,15 +1,7 @@
-<?php include(locate_template('header.php')); ?>
-
-<?php while ( have_posts() ) { the_post(); ?>
-      
-    <h1>
-      <?php the_title(); ?>
-    </h1>
-    
-    <div>
-      <?php the_content(); ?>
-    </div>
-  
-<?php } ?>
-
-<?php include(locate_template('footer.php')); ?>
+<?php 
+include(locate_template('header.php'));
+while ( have_posts() ) { the_post();
+  the_content();
+}
+include(locate_template('footer.php')); 
+?>
