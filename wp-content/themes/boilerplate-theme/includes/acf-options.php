9ab96c047aca152c23412181a556dc97d865eecc
Stephen Griffin <notepadwebdev@gmail.com>
2025-04-23 11:37:11 +0100

    Wrap ACF Options incl inside acf/init action.

--- wp-content/themes/boilerplate-theme/includes/acf-options.php
+++ wp-content/themes/boilerplate-theme/includes/acf-options.php
@@ -2,44 +2,46 @@
 /**
  * ACF Options.
  */
-if( function_exists('acf_add_options_page') ) {
-	acf_add_options_page(array(
-		'page_title' 	=> 'Theme General Settings',
-		'menu_title'	=> 'Theme Settings',
-		'menu_slug' 	=> 'theme-general-settings',
-		'capability'	=> 'edit_posts',
-		'redirect'		=> false,
-    'show_in_graphql' => true
-  ));
+add_action('acf/init', function() { 
+	if( function_exists('acf_add_options_page') ) {
+		acf_add_options_page(array(
+			'page_title' 	=> 'Theme General Settings',
+			'menu_title'	=> 'Theme Settings',
+			'menu_slug' 	=> 'theme-general-settings',
+			'capability'	=> 'edit_posts',
+			'redirect'		=> false,
+			'show_in_graphql' => true
+		));
 
-	acf_add_options_sub_page(array(
-		'page_title' 	=> 'Contact Details',
-		'menu_title'	=> 'Contact Details',
-		'parent_slug'	=> 'theme-general-settings',
-	));
+		acf_add_options_sub_page(array(
+			'page_title' 	=> 'Contact Details',
+			'menu_title'	=> 'Contact Details',
+			'parent_slug'	=> 'theme-general-settings',
+		));
 
-	acf_add_options_sub_page(array(
-		'page_title' 	=> 'Social Links',
-		'menu_title'	=> 'Social Links',
-		'parent_slug'	=> 'theme-general-settings',
-	));
+		acf_add_options_sub_page(array(
+			'page_title' 	=> 'Social Links',
+			'menu_title'	=> 'Social Links',
+			'parent_slug'	=> 'theme-general-settings',
+		));
 
-	acf_add_options_sub_page(array(
-		'page_title' 	=> 'Header Settings',
-		'menu_title'	=> 'Header Settings',
-		'parent_slug'	=> 'theme-general-settings',
-	));
+		acf_add_options_sub_page(array(
+			'page_title' 	=> 'Header Settings',
+			'menu_title'	=> 'Header Settings',
+			'parent_slug'	=> 'theme-general-settings',
+		));
 
-	acf_add_options_sub_page(array(
-		'page_title' 	=> 'Footer Settings',
-		'menu_title'	=> 'Footer Settings',
-		'parent_slug'	=> 'theme-general-settings',
-	));
+		acf_add_options_sub_page(array(
+			'page_title' 	=> 'Footer Settings',
+			'menu_title'	=> 'Footer Settings',
+			'parent_slug'	=> 'theme-general-settings',
+		));
 
-	acf_add_options_sub_page(array(
-		'page_title' 	=> '404 Page',
-		'menu_title'	=> '404 Page',
-		'parent_slug'	=> 'theme-general-settings',
-	));
-}
+		acf_add_options_sub_page(array(
+			'page_title' 	=> '404 Page',
+			'menu_title'	=> '404 Page',
+			'parent_slug'	=> 'theme-general-settings',
+		));
+	}
+});
 ?>
